5f4eaa7ac0aeb177d979542f656983ca656910fa
WillyWill143 <125220967+WillyWill143@users.noreply.github.com>
2023-12-25 12:35:19 +0200

    Create deep.py

---
+++ week1/deep.py
@@ -0,0 +1,6 @@
+z  = input("What is the answer to the Great Queation of Life, the Universe, and Everything? ").lower().strip()
+
+if z in ["42", "forty-two", "forty two"]:
+    print("yes")
+else:
+    print("No")
