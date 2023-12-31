ace5fb410113516dbf03df06f2ece09e13dfa1bf
WillyWill143 <125220967+WillyWill143@users.noreply.github.com>
2023-12-31 12:16:33 +0200

    Create bank.py

---
+++ week5/test_bank/bank.py
@@ -0,0 +1,18 @@
+def main():
+    greets = (input("Greeting: ")).strip()
+    print(f"${value(greets)}")
+
+
+def value(greeting):
+    greeting = greeting.lower()
+    if greeting.startswith("hello"):
+        return 0
+    elif greeting.startswith("h"):
+        return 20
+    else:
+        return 100
+
+
+
+if __name__ == "__main__":
+    main()
