9ca2770d316bb7cb5b0536865b704c9a216adfdf
WillyWill143 <125220967+WillyWill143@users.noreply.github.com>
2024-04-08 05:07:16 +0200

    Update deep.py

--- week1/deep.py
+++ week1/deep.py
@@ -4,3 +4,20 @@ if z in ["42", "forty-two", "forty two"]:
     print("yes")
 else:
     print("No")
+
+
+##or
+
+def main():
+    answer = input("What is the answer to the Great Question of Life, the Universe and Everything? ").lower().strip()
+    fort(answer)
+
+def fort(z):
+    if z == "42" or z == "forty two" or z == "forty-two":
+        print("Yes")
+    else:
+        print("No")
+
+
+
+main()
