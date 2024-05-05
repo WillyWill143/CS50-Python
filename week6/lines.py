ddd6d46177492fabd096bb3c947dd7ed4f1da861
WillyWill143 <125220967+WillyWill143@users.noreply.github.com>
2024-05-05 21:57:44 +0300

    Update lines.py

--- week6/lines.py
+++ week6/lines.py
@@ -30,3 +30,33 @@ def count_line(file):
 if __name__ == "__main__":
     main()
 
+
+
+####or
+
+import sys
+
+def main():
+
+    if len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    elif len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    elif not sys.argv[1].endswith(".py"):
+        sys.exit("Not a python file")
+    else:
+        print(count_lines(sys.argv[1]))
+
+def count_lines(file):
+    try:
+        lines = []
+        with open(file) as file:
+            for line in file:
+                if not (line.lstrip().startswith("#") or line.isspace()):
+                    lines.append(line.strip())
+    except FileNotFoundError:
+        sys.exit("File does not exist")
+    return len(lines)
+
+if __name__ == "__main__":
+    main()
